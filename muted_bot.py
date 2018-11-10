2beac69aea43a11de53ab179df1b2b556120cdf2
Oisin <oisinhenry@gmail.com>
2018-11-10 20:42:29 +0000

    new features

--- muted_bot.py
+++ muted_bot.py
@@ -16,14 +16,25 @@ async def on_message(message):
     if message.content.startswith("!test"):
         await client.send_message(message.channel, "test")
 
-
+# notify users when they mute themselves
 @client.event
 async def on_voice_state_update(before, after):
     print("voice state changed")
     muted_user = after
     str_mention = muted_user.mention
     if muted_user.voice.self_mute:
-        await client .send_message(client.get_channel("157183056413851650"), "{}, you're muted".format(str_mention))
+        await client.send_message(client.get_channel("157183056413851650"), "{}, you're muted".format(str_mention))
+
+# ping when a user comes online
+@client.event
+async def on_member_update(before,after):
+    if str(before.status) == "offline":
+        if str(after.status) == "online" or str(after.status) == "idle":
+            await client.send_message(client.get_channel("157183056413851650"), "{} just came online.".format(after.mention))
 
 
+@client.event
+async def on_message(message):
+    if message.content.startswith("forsenE"):
+        await client.send_message(message.channel, "<:forsen1:442133101896925194><:forsen2:442133102131806208>\n<:forsen3:442133101905444864><:forsen4:442133101959970816>")
 client.run("NTEwNTY5ODMwNzQzODAxODk1.DseRCA.qMJpNtao5jFXGXrG0RusByeqFGY")
